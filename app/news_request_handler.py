84a36aef0bb652a2aeada709f1001a2729caa370
kamauvick <kamauvick@outlook.com>
2019-09-07 19:29:59 +0300

    Add logic to the sources function.

--- app/news_request_handler.py
+++ app/news_request_handler.py
@@ -7,7 +7,12 @@ class NewsRequest:
         self.API_KEY = Config.API_KEY
 
     def get_sources(self):
-        pass
+        sources = []
+        sources_url = 'https://newsapi.org/v2/sources?q={}&apiKey={}'.format(sources, self.API_KEY)
+        response = requests.get(sources_url)
+        if response.status_code == 200:
+            for data in response.json()['sources']:
+                pass
 
     def get_news(self, source):
         pass
