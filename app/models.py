3b50d486ddf2cf7eb38b0a5f90fe02e4b4be8d13
kamauvick <kamauvick@outlook.com>
2019-09-08 19:44:56 +0300

    Add a model class for news articles and inherit from News class.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
+""" Model class for news """
+
+
 class News:
-    def __init__(self, source):
+    def __init__(self, source, urlToImage, description, title, author):
+        self.urlToImage = urlToImage
+        self.description = description
+        self.title = title
+        self.author = author
         self.source = source
+
+
+""" Model class for articles which inherits from news """
+
+
+class Articles(News):
+    def __init__(self):
+        super(News, self).__init__()
